746b77234a6e465185ad4493ec57416a7331850d
Samuel Bret <samuelbret@icloud.com>
2025-10-03 21:10:25 +0100

    adding the arctan stuff

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -5,18 +5,14 @@ uplimit = 2*np.pi
 p = 0-uplimit
 increment = 1/7
 x = np.linspace(0-uplimit, uplimit, 100)
-plt.figure(figsize=(5, 2.7), layout='constrained')
+plt.figure(figsize=(5, 5), layout='constrained')
 
 
 while(p < uplimit):
     a = np.cos(p)
     b = np.sin(p) - np.cos(p) * p
-    plt.plot(x, a*x + b, color="black", linewidth="0.5")
-    p += increment / (0.3 + np.abs(0 - np.sin(p)))
-
-p = 0-uplimit
-increment= 1/10
-
+    plt.plot(x, a*x + b, color="black", linewidth="0.2")
+    p += increment / (1 + abs(1/(1 + a**2) * (-np.sin(p))))
 
 
 plt.show()
