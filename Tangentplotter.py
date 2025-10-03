0eba04b296d173e3f94d2c90833d2693638cd64d
Samuel Bret <samuelbret@icloud.com>
2025-10-03 18:48:38 +0100

    using f''(x) to space tan

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -1,17 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-p = 0.0
-uplimit = 11*np.pi
-increment = 1/10
-x = np.linspace(0, uplimit, 100)
+uplimit = 2*np.pi
+p = 0-uplimit
+increment = 1/7
+x = np.linspace(0-uplimit, uplimit, 100)
 plt.figure(figsize=(5, 2.7), layout='constrained')
 
 
 while(p < uplimit):
     a = np.cos(p)
     b = np.sin(p) - np.cos(p) * p
-    plt.plot(x, a*x + b, color="black", linewidth="0.2")
-    p += increment
+    plt.plot(x, a*x + b, color="black", linewidth="0.5")
+    p += increment / (0.3 + np.abs(0 - np.sin(p)))
+
+p = 0-uplimit
+increment= 1/10
+
+
 
 plt.show()
+
