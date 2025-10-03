a14f3913da363d8d94425f2a5e931a06ace4e055
Samuel Bret <samuelbret@icloud.com>
2025-10-03 22:16:39 +0100

    Refining increment fuction

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-uplimit = 2*np.pi
+uplimit = 10*np.pi
 p = 0 - uplimit
-increment = 1/10
+increment = 1/30
 x = np.linspace( 0 - uplimit, uplimit, 100)
-plt.figure(figsize=(5, 2.7), layout='constrained')
+plt.figure(figsize=(5, 5), layout='constrained')
 
 
 while(p < uplimit):
     a = 1 - ((p**2) / 2) + ((p**4) / 24)
     b = (2 * (p**3) / 6) - (4 * (p**5) / 120)
     plt.plot(x, (a*x) + b, color="black", linewidth="0.2")
-    p += increment / (1 + abs(1/(1 + a**2) * (0 - p + ((p**3)/6))))
+    p += increment / (0.1 + abs(1/(1 + a**2) * (0 - p + ((p**3)/6))))
 
+plt.axis("equal")
 plt.show()
