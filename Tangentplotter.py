5618ffcf69bda2161d066c6fb5e60f054a9b49bd
Samuel Bret <samuelbret@icloud.com>
2025-10-03 22:45:55 +0100

    stuff

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-uplimit = 2*np.pi
+uplimit = 5*np.pi
 p = 0-uplimit
-increment = 1/7
+increment = 1/30
 x = np.linspace(0-uplimit, uplimit, 100)
 plt.figure(figsize=(5, 5), layout='constrained')
 
@@ -12,8 +12,16 @@ while(p < uplimit):
     a = np.cos(p)
     b = np.sin(p) - np.cos(p) * p
     plt.plot(x, a*x + b, color="black", linewidth="0.2")
-    p += increment / (1 + abs(1/(1 + a**2) * (-np.sin(p))))
+    p += increment / (0.1 + abs(1/(1 + a**2) * (-np.sin(p))))
 
+p = 0-uplimit
 
+while(p < uplimit):
+    a = 1 - ((p**2) / 2) + ((p**4) / 24) - ((p**6) / 720)
+    b = (2 * (p**3) / 6) - (4 * (p**5) / 120) + (6 * (p**7) / 5040)
+    plt.plot(x, (a*x) + b, color="red", linewidth="0.2")
+    p += increment / (0.1 + abs(1/(1 + a**2) * (0 - p + ((p**3)/6))))
+
+plt.axis("equal")
 plt.show()
 
