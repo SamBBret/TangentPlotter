b3449d964c59b22e2e40b69c0f9733e9b2e7fbfd
Samuel Bret <samuelbret@icloud.com>
2025-10-04 21:49:02 +0100

    making it beautifull

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as sci
 
-uplimit = 5*np.pi
-p = 0-uplimit
-increment = 1/10
+uplimitp = 7.1
+uplimit = 20
+p = 0-uplimitp
+increment = 1/30
 x = np.linspace(0-uplimit, uplimit, 100)
 plt.figure(figsize=(5, 5), layout='constrained')
 
 
-while(p < uplimit):
+while(p < uplimitp):
     a = sci.gamma(p) * sci.digamma(p)
     b = sci.gamma(p) - (a * p)
     plt.plot(x, a*x + b, color="black", linewidth="0.2")
-    p += increment #/ (0.2 + abs(1/(1 + a**2) * (a + (sci.gamma(p) * sci.polygamma(1, p)))))
+    p += increment / (0.5 + abs(1/(1 + a**2) * (a + (sci.gamma(p) * sci.polygamma(1, p)))))
     
 plt.axis("equal")
 plt.xlim(-100, 100)
