30ac1acc56735ab242e75b7e1aa429e607cd9b77
Samuel Bret <samuelbret@icloud.com>
2025-10-05 21:25:21 +0100

    it's working

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -1,17 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+functionlimit = 5*np.pi
 uplimit = 5*np.pi
-p = 0-uplimit
+sidelimit = 6*np.pi
+p = 0-functionlimit
 increment = 1/30
-x = np.linspace(0-uplimit, uplimit, 100)
 plt.figure(figsize=(5, 5), layout='constrained')
 
-
-while(p < uplimit):
+while(p < functionlimit):
+    xpoint = []
+    ypoint = []
     a = np.cos(p)
-    b = np.sin(p) - np.cos(p) * p
-    plt.plot(x, a*x + b, color="black", linewidth="0.2")
+    c = np.sin(p) - a * p
+    b = a * (-sidelimit) + c
+    if abs(b) <= uplimit:
+        xpoint.append(-sidelimit)
+        ypoint.append(b)
+    b = a * sidelimit + c
+    if abs(b) <= uplimit:
+        xpoint.append(sidelimit)
+        ypoint.append(b)
+    b = (-uplimit - c) / a
+    if abs(b) < sidelimit:
+        xpoint.append(b)
+        ypoint.append(-uplimit)
+    b = (uplimit - c) / a
+    if abs(b) < sidelimit:
+        xpoint.append(b)
+        ypoint.append(uplimit)
+
+    plt.plot(xpoint, ypoint, color="black", linewidth="0.2")
     p += increment / (0.1 + abs(1/(1 + a**2) * (-np.sin(p))))
     
 plt.axis("equal")
