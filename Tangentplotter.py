ceeaf2df14405a62a08e9a9ea2773b2c8e5bf450
Samuel Bret <samuelbret@icloud.com>
2026-03-20 14:14:46 +0000

    i dont know

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -17,17 +17,21 @@ xreallenght = 1000
 
 halfnailwidth = 0.6 / 2
 
-zoom = 0.014
-functionlimit = 7*np.pi
+zoom = 0.013
+functionlimit = 7.99
 uplimit = ysectors*zoom
 sidelimit = xsectors*zoom 
 p = -7.99
-increment = 1/30
+increment = 1/40
 fig, ax = plt.subplots(figsize=(5, 5), layout='constrained')
+
+
 
 sectors_to_mm = xreallenght / xsectors
 coord_to_mm  = xreallenght / sidelimit / 2
 coord_to_sectors = xsectors / sidelimit / 2
+
+string_lenght = 0
 
 state = {'index': 0}
 lines = []
@@ -42,10 +46,11 @@ while(p < functionlimit):
     points = []
     a = sci.digamma(p) * sci.gamma(p)
     b = sci.gamma(p) - a * p
+
     c = (0.25 + abs(1/(1 + a**2) * (a + (sci.gamma(p) * sci.polygamma(1, p)))))
-    d = a * (-sidelimit) + b
 
     d = a * (-sidelimit) + b
+
     if abs(d) <= uplimit:
         points.append(Vector2(-sidelimit, d))
 
@@ -61,7 +66,10 @@ while(p < functionlimit):
     if abs(d) < sidelimit:
         points.append(Vector2(d, uplimit))
 
-    lines.append(points)
+    if len(points) != 0:
+     
+        lines.append(points)
+
     p += increment / c
 
 for line in lines:
@@ -72,15 +80,15 @@ for line in lines:
             if p.y - round(((p.y / ysectors) + 0.5) * ysectors) + ysectors/2 + .5 >= 1 - halfnailwidth:
             
                 p.y = round(((p.y / ysectors) + 0.5) * ysectors - (ysectors/2)) * sectors_to_mm
-                print(p.y)
+
             elif p.y - round(((p.y /( 2 * uplimit)) + 0.5) * ysectors) + ysectors/2 + .5 <  .5 - halfnailwidth:
 
                  p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + 1 - (ysectors/2)) * sectors_to_mm)
-                 print("ola")
+                 
             else:
                 
                  p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + (1 - halfnailwidth * 2) - (ysectors/2)) * sectors_to_mm)
-                 print("hi")
+                 
 
         elif p.x == sidelimit:
             p.x = p.x * coord_to_mm
@@ -88,15 +96,15 @@ for line in lines:
             if p.y - round(((p.y / ysectors) + 0.5) * ysectors) + ysectors/2 + .5 < halfnailwidth:
             
                 p.y = round(((p.y / ysectors) + 0.5) * ysectors - (ysectors/2)) * sectors_to_mm
-                print(p.y)
+
             elif p.y - round(((p.y /( 2 * uplimit)) + 0.5) * ysectors) + ysectors/2 + .5 >=  .5 + halfnailwidth:
 
                  p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + 1 - (ysectors/2)) * sectors_to_mm)
-                 print("ola")
+             
             else:
                 
                  p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + (halfnailwidth * 2) - (ysectors/2)) * sectors_to_mm)
-                 print("hi")
+                 
         
                  
         elif p.y == uplimit:
@@ -117,6 +125,10 @@ for line in lines:
         else:
             p.y = p.y * coord_to_mm
             p.x = p.x * coord_to_sectors
+
+            #p.y = p.y - ((5/1000) * (p.x + 500))
+
+
             if p.x - round(((p.x / xsectors) + 0.5) * xsectors) + xsectors/2 + .5 < halfnailwidth:
 
                 p.x = (round(((p.x / xsectors) + 0.5) * xsectors - (xsectors/2)) * sectors_to_mm)
@@ -128,19 +140,32 @@ for line in lines:
             else:
                 
                  p.x = ((round(((p.x / xsectors) + 0.5) * xsectors) + (halfnailwidth * 2) - (xsectors/2)) * sectors_to_mm)
+    string_lenght += np.sqrt(np.pow(line[0].x - line[1].x, 2) + np.pow(line[0].y - line[1].y, 2))
+
 
 plt.axis("equal")  
 print(len(lines))
+print(str (string_lenght / 1000) + "m")
 
-def on_key(event):
-    if event.key == " " and len(lines) >  state['index']:
+
+def draw_line():
+    if len(lines) >  state['index']:
         ax.plot([lines[state['index']][0].x, lines[state['index']][1].x], [lines[state['index']][0].y, lines[state['index']][1].y], color="black", linewidth="0.2")
         plt.draw()
         print(str(state['index']) + ":")
         print("Ponto 0:   " + str(round(lines[state['index']][0].x)) + "     " + str(round(lines[state['index']][0].y)))
         print("Ponto 1:   " + str(round(lines[state['index']][1].x)) + "     " + str(round(lines[state['index']][1].y)))
 
-        state['index'] += 1
+    state['index'] += 1
+
+def on_key(event):
+    if event.key == " ":
+        draw_line()
+    if event.key == "a":
+        while state['index'] < len(lines) :
+            draw_line()
+        print(len(lines))
+        print(str (string_lenght / 1000) + "m")
 
 ax.set_xlim((-(xsectors * sectors_to_mm)/2 - 20), ((xsectors * sectors_to_mm)/2 + 20))
 ax.set_ylim((-(ysectors * sectors_to_mm)/2 - 20), ((ysectors * sectors_to_mm)/2 + 20))
