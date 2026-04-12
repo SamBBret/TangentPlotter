b12ee99f84de72fdecfd175f989554f90c24ea71
Samuel Bret <samuelbret@icloud.com>
2026-04-12 17:05:19 +0100

    improved increment mechanism

--- Tangentplotter.py
+++ Tangentplotter.py
@@ -10,19 +10,20 @@ class Vector2:
         self.y = y
 
 
-xsectors = 500
-ysectors = 400
+xreallenght = 1002
+yreallenght = 802
 
-xreallenght = 1000
+xsectors = 501
+ysectors = xsectors * yreallenght / xreallenght
 
 halfnailwidth = 0.6 / 2
 
 zoom = 0.013
-functionlimit = 7.99
+functionlimit = 7.98
 uplimit = ysectors*zoom
 sidelimit = xsectors*zoom 
-p = -7.99
-increment = 1/40
+p = -7.98
+increment = 1/42
 fig, ax = plt.subplots(figsize=(5, 5), layout='constrained')
 
 
@@ -39,7 +40,7 @@ lines = []
 print("0-gamma")
 print("1-sin")
 
-functiontoDraw = input("Function to draw:")
+#functiontoDraw = input("Function to draw:")
 
 while(p < functionlimit):
 
@@ -47,10 +48,7 @@ while(p < functionlimit):
     a = sci.digamma(p) * sci.gamma(p)
     b = sci.gamma(p) - a * p
 
-    c = (0.25 + abs(1/(1 + a**2) * (a + (sci.gamma(p) * sci.polygamma(1, p)))))
-
     d = a * (-sidelimit) + b
-
     if abs(d) <= uplimit:
         points.append(Vector2(-sidelimit, d))
 
@@ -70,76 +68,22 @@ while(p < functionlimit):
      
         lines.append(points)
 
-    p += increment / c
+    c = (0.25 + abs(1/(1 + a**2) * (a + (sci.gamma(p) * sci.polygamma(1, p)))))
+    
+    dp = increment / c
+
+    a = sci.digamma(p + dp) * sci.gamma(p + dp)
+
+    c1 = (0.25 + abs(1/(1 + (a)**2) * (a + (sci.gamma(p + dp) * sci.polygamma(1, p + dp)))))
+
+    p += increment / ((c + c1) / 2)
+
 
 for line in lines:
     for p in line:
-        if p.x == -sidelimit:
-            p.x = p.x * coord_to_mm
-            p.y = p.y * coord_to_sectors 
-            if p.y - round(((p.y / ysectors) + 0.5) * ysectors) + ysectors/2 + .5 >= 1 - halfnailwidth:
-            
-                p.y = round(((p.y / ysectors) + 0.5) * ysectors - (ysectors/2)) * sectors_to_mm
-
-            elif p.y - round(((p.y /( 2 * uplimit)) + 0.5) * ysectors) + ysectors/2 + .5 <  .5 - halfnailwidth:
-
-                 p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + 1 - (ysectors/2)) * sectors_to_mm)
-                 
-            else:
-                
-                 p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + (1 - halfnailwidth * 2) - (ysectors/2)) * sectors_to_mm)
-                 
-
-        elif p.x == sidelimit:
-            p.x = p.x * coord_to_mm
-            p.y = p.y * coord_to_sectors 
-            if p.y - round(((p.y / ysectors) + 0.5) * ysectors) + ysectors/2 + .5 < halfnailwidth:
-            
-                p.y = round(((p.y / ysectors) + 0.5) * ysectors - (ysectors/2)) * sectors_to_mm
-
-            elif p.y - round(((p.y /( 2 * uplimit)) + 0.5) * ysectors) + ysectors/2 + .5 >=  .5 + halfnailwidth:
-
-                 p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + 1 - (ysectors/2)) * sectors_to_mm)
-             
-            else:
-                
-                 p.y = ((round(((p.y / ysectors) + 0.5) * ysectors) + (halfnailwidth * 2) - (ysectors/2)) * sectors_to_mm)
-                 
-        
-                 
-        elif p.y == uplimit:
-            p.y = p.y * coord_to_mm
-            p.x = p.x * coord_to_sectors
-            if p.x - round(((p.x / xsectors) + 0.5) * xsectors) + xsectors/2 + .5 >= 1 - halfnailwidth:
-
-                p.x = (round(((p.x / xsectors) + 0.5) * xsectors - (xsectors/2)) * sectors_to_mm)
-
-            elif p.x - round(((p.x / xsectors) + 0.5) * xsectors) + xsectors/2 + .5 < .5 - halfnailwidth:
-
-                 p.x = ((round(((p.x / xsectors) + 0.5) * xsectors) + 1 - (xsectors/2)) * sectors_to_mm)
-
-            else:
-                
-                 p.x = ((round(((p.x / xsectors) + 0.5) * xsectors) + (1 - halfnailwidth * 2) - (xsectors/2)) * sectors_to_mm)
+        p.x = p.x * coord_to_mm
+        p.y = p.y * coord_to_mm
  
-        else:
-            p.y = p.y * coord_to_mm
-            p.x = p.x * coord_to_sectors
-
-            #p.y = p.y - ((5/1000) * (p.x + 500))
-
-
-            if p.x - round(((p.x / xsectors) + 0.5) * xsectors) + xsectors/2 + .5 < halfnailwidth:
-
-                p.x = (round(((p.x / xsectors) + 0.5) * xsectors - (xsectors/2)) * sectors_to_mm)
-
-            elif p.x - round(((p.x / xsectors) + 0.5) * xsectors) + xsectors/2 + .5 >= .5 + halfnailwidth:
-
-                 p.x = ((round(((p.x / xsectors) + 0.5) * xsectors) + 1 - (xsectors/2)) * sectors_to_mm)
-
-            else:
-                
-                 p.x = ((round(((p.x / xsectors) + 0.5) * xsectors) + (halfnailwidth * 2) - (xsectors/2)) * sectors_to_mm)
     string_lenght += np.sqrt(np.pow(line[0].x - line[1].x, 2) + np.pow(line[0].y - line[1].y, 2))
 
 
@@ -153,8 +97,8 @@ def draw_line():
         ax.plot([lines[state['index']][0].x, lines[state['index']][1].x], [lines[state['index']][0].y, lines[state['index']][1].y], color="black", linewidth="0.2")
         plt.draw()
         print(str(state['index']) + ":")
-        print("Ponto 0:   " + str(round(lines[state['index']][0].x)) + "     " + str(round(lines[state['index']][0].y)))
-        print("Ponto 1:   " + str(round(lines[state['index']][1].x)) + "     " + str(round(lines[state['index']][1].y)))
+        print("Ponto 0:   " + str(round(lines[state['index']][0].x + (xreallenght / 2), 1)) + "     " + str(round(lines[state['index']][0].y + (yreallenght / 2), 1)))
+        print("Ponto 1:   " + str(round(lines[state['index']][1].x + (xreallenght / 2), 1)) + "     " + str(round(lines[state['index']][1].y + (yreallenght / 2), 1)))
 
     state['index'] += 1
 
